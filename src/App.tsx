8ac1835055b5bbaa6a8384fdf95f747a6a889863
matiassalicru <mattiassalicru@gmail.com>
2022-08-25 08:38:49 -0300

    feat: implement language switcher

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,31 @@ import { About } from './sections/AboutSection/About/About'
 // Styled-components
 import GlobalCSS from './global.css'
 import { ThemeProvider } from 'styled-components'
+import { SCFixedButtons, SCThemeButton } from './styles'
 
 // Context
 import { ThemeContext } from './context/ThemeContext/Context'
 
+// Constants
+import { DARK } from './context/ThemeContext/constants'
+import { ENG_LANG, ESP_LANG } from './lang/constants'
+
+// Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCloudSun,
+  faGlobeAmericas,
+  faMoon,
+} from '@fortawesome/free-solid-svg-icons'
+import { useTranslation } from 'react-i18next'
+
 export const App = () => {
-  const { theme } = useContext(ThemeContext)
+  const { theme, changeTheme } = useContext(ThemeContext)
+  const { i18n } = useTranslation()
+
+  const changeLanguage = () => {
+    i18n.changeLanguage(i18n.language === ENG_LANG ? ESP_LANG : ENG_LANG)
+  }
   return (
     <ThemeProvider theme={theme}>
       <GlobalCSS />
@@ -24,6 +43,17 @@ export const App = () => {
           <Route path='/about' element={<About />} />
         </Routes>
       </Router>
+      <SCFixedButtons>
+        <SCThemeButton onClick={changeTheme} type='button'>
+          <FontAwesomeIcon
+            icon={theme?.theme === DARK ? faCloudSun : faMoon}
+            size='lg'
+          />
+        </SCThemeButton>
+        <SCThemeButton onClick={changeLanguage} type='button'>
+          <FontAwesomeIcon icon={faGlobeAmericas} size='lg' />
+        </SCThemeButton>
+      </SCFixedButtons>
     </ThemeProvider>
   )
 }
---
+++ src/styles.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const SCFixedButtons = styled.div`
+  display: flex;
+  position: fixed;
+  display: flex;
+  flex-direction: row-reverse;
+  bottom: 30px;
+  right: 20px;
+  width: fit-content;
+  gap: 15px;
+  z-index: 3;
+`
+
+export const SCThemeButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: fit-content;
+  min-width: 40px;
+  height: 40px;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 5px;
+  border: 1px solid ${({ theme }) => theme.borderColor};
+  background-color: ${({ theme }) => theme.backgroundColor};
+  color: ${({ theme }) => theme.color};
+  transition: all 0.5s;
+
+  &:active {
+    box-shadow: 2px 2px 2px ${({ theme }) => theme.color};
+  }
+`
